something wrong when switching rotation mode

layers/modules/sph_multibox_loss.py
@@ -54,7 +54,7 @@ class SphMultiBoxLoss(nn.Module):
             ground_truth (tensor): Ground truth boxes and labels for a batch,
                 shape: [batch_size,num_objs,6] (last idx is the label).
         """
-        if self.no_rotation:
+        if len(predictions) == 3:
             loc_data, conf_data, priors = predictions
         else:
             loc_data, conf_data, priors, rot_data = predictions
@@ -68,11 +68,11 @@ class SphMultiBoxLoss(nn.Module):
             if self.use_gpu:
                 loc_t = torch.cuda.FloatTensor(num, num_priors, 4)
                 conf_t = torch.cuda.LongTensor(num, num_priors)
-                loc_t = torch.cuda.FloatTensor(num, num_priors, 1)
+                rot_t = torch.cuda.FloatTensor(num, num_priors)
             else:
                 loc_t = torch.Tensor(num, num_priors, 4)
                 conf_t = torch.LongTensor(num, num_priors)
-                loc_t = torch.Tensor(num, num_priors, 1)
+                rot_t = torch.Tensor(num, num_priors, 1)
             for idx in range(num):
                 truths = targets[idx][:, :-1].data # box annotation
                 labels = targets[idx][:, -1].data # action type
@@ -95,12 +95,12 @@ class SphMultiBoxLoss(nn.Module):
         loc_p = loc_data[pos_idx].view(-1, 4)
         loc_t = loc_t[pos_idx].view(-1, 4)
         loss_l = F.smooth_l1_loss(loc_p[:,:4], loc_t[:,:4], reduction='sum')
-        if self.no_rotation:
+        if len(predictions) == 3:
             loss_ro = 0
         else:
-            rot_p = rot_data[pos_idx].view(-1, 1)
-            rot_t = rot_t[pos_idx].view(-1, 1)
-            loss_ro = F.smooth_l1_loss(rot_p[:,4], rot_t[:,4], reduction='sum')
+            rot_p = rot_data[pos].view(-1, 1)
+            rot_t = rot_t[pos].view(-1, 1)
+            loss_ro = F.smooth_l1_loss(rot_p, rot_t, reduction='sum')
         with torch.no_grad():
             # Compute max conf across batch for hard negative mining
             batch_conf = conf_data.view(-1, self.num_classes)

model/fpnssd/net.py
@@ -16,6 +16,7 @@ class FPNSSD512(nn.Module):
         self.num_classes = num_classes
         self.num_anchors = (4, 6, 6, 6, 6, 4, 4)
         self.in_channels = (256, 256, 256, 256, 256, 256, 256)
+        self.cfg = cfg
 
         self.extractor = FPN50()
         priorbox = SphPriorBox(cfg)
@@ -48,15 +49,15 @@ class FPNSSD512(nn.Module):
         loc_preds = torch.cat(loc_preds, 1)
         cls_preds = torch.cat(cls_preds, 1)
 
-        if not cfg['no_rotation']:
+        if not self.cfg['no_rotation']:
             rot_preds = []
             for i, x in enumerate(xs):
                 rot_pred = self.rot_layers[i](x)
                 rot_pred = rot_pred.permute(0,2,3,1).contiguous()
-                rot_preds.append(rot_pred.view(rot_pred.size(0),-1,1))
+                rot_preds.append(rot_pred.view(rot_pred.size(0),-1))
             rot_preds = torch.cat(rot_preds, 1)
 
-        if cfg['no_rotation']:
+        if self.cfg['no_rotation']:
             return loc_preds, cls_preds, self.priors
         else:
             return loc_preds, cls_preds, self.priors, rot_preds 

train-ucf24-test.py
@@ -86,7 +86,7 @@ def main():
     args.num_classes = num_classes
     args.stepvalues = [int(val) for val in args.stepvalues.split(',')]
     args.loss_reset_step = 30
-    args.eval_step = 5000
+    args.eval_step = 1000
     args.print_step = 10
 
     ## Define the experiment Name will used to same directory and ENV for visdom
@@ -224,7 +224,7 @@ def train(args, net, optimizer, criterion, scheduler):
             scheduler.step()
             loc_loss = loss_l.item()
             conf_loss = loss_c.item()
-            rot_loss = loss_r.item()
+            rot_loss = 0 if loss_r == 0 else loss_r.item() 
             # print('Loss data type ',type(loc_loss))
             loc_losses.update(loc_loss)
             cls_losses.update(conf_loss)
@@ -289,7 +289,6 @@ def validate(args, net, val_data_loader, val_dataset, iteration_num, iou_thresh=
     print('Validating at ', iteration_num)
     num_images = len(val_dataset)
     num_classes = args.num_classes
-    box_len = 4 if args.cfg['no_rotation'] else 5
 
     det_boxes = [[] for _ in range(len(CLASSES))]
     gt_boxes = []
@@ -346,7 +345,7 @@ def validate(args, net, val_data_loader, val_dataset, iteration_num, iou_thresh=
                         continue
                     boxes = decoded_boxes.clone()
                     l_mask = c_mask.unsqueeze(1).expand_as(boxes)
-                    boxes = boxes[l_mask].view(-1, box_len)
+                    boxes = boxes[l_mask].view(-1, 4)
                     # idx of highest scoring and non-overlapping boxes per class
                     ids, counts = nms(boxes, scores, args.nms_thresh, args.topk)  # idsn - ids after nms
                     scores = scores[ids[:counts]].cpu().numpy()
@@ -371,6 +370,7 @@ def validate(args, net, val_data_loader, val_dataset, iteration_num, iou_thresh=
                 torch.cuda.synchronize()
                 te = time.perf_counter()
                 print('NMS stuff Time {:0.3f}'.format(te - tf))
+            if val_itr > 500:break
     print('Evaluating detections for itration number ', iteration_num)
     return evaluate_detections(gt_boxes, det_boxes, CLASSES, iou_thresh=iou_thresh)
 